c8042f5767b5f5bb3d7c4b11ee15292c69586707
Vahid Najafi <Vahidnajafi.work@gmail.com>
2022-11-10 20:06:04 -0500

    Initializing projects.

---
+++ src/back-end/MicroFrontend.Api.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace MicroFrontend.Api.IntegrationTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
