fa25f8d89a891cd8ff6ece754aced075e2ab0804
Vahid Najafi <Vahidnajafi.work@gmail.com>
2022-11-10 21:07:00 -0500

    Add swagger documentation to upsert endpoint.

--- src/back-end/MicroFrontend.Api/Program.cs
+++ src/back-end/MicroFrontend.Api/Program.cs
@@ -3,13 +3,14 @@ using MicroFrontend.Api.Common.Models;
 using MicroFrontend.Api.Persistence;
 using MicroFrontend.Api.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Annotations;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => options.EnableAnnotations());
 
 // Setting and providing db context
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -18,7 +19,11 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString);
 });
 builder.Services.AddTransient<IAppDbContext>(provider => provider.GetService<AppDbContext>() ?? throw new InvalidOperationException());
+
+// Other tools
 builder.Services.AddAutoMapper(typeof(Program));
+
+// Services
 builder.Services.AddTransient<IUserService, UserService>();
 
 var app = builder.Build();
@@ -38,7 +43,8 @@ app.MapGet(usersUrl, async (IUserService repo) => await repo.GetAsync());
 app.MapGet(usersUrl + "/{id}", 
     async (IUserService repo, string id) => await repo.GetByIdAsync(id));
 app.MapPost(usersUrl,
-    async (IUserService repo, UserDto requestBody) => await repo.UpsertAsync(requestBody));
+    async (IUserService repo, UserDto requestBody) => await repo.UpsertAsync(requestBody))
+    .WithMetadata(new SwaggerOperationAttribute(summary: "Upsert (covers both insert and update)", description: "Send null as the id to do insertion, if you fill the id field it will try to update (edit)."));
 app.MapDelete(usersUrl + "/{id}", async (IUserService repo, string id) => await repo.DeleteAsync(id));
 
 app.Run();
